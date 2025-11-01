986df457ab60672d01f1a3d3e3e4330e3e48b736
Shandy Ariady <shandylatoz851@gmail.com>
2025-11-01 11:39:05 +0700

    make test error

--- src/states/authUser/reducer.test.js
+++ src/states/authUser/reducer.test.js
@@ -26,7 +26,7 @@ describe('authUserReducer function', () => {
     const nextState = authUserReducer(initialState, action);
 
     // Assert
-    expect(nextState).toBe(initialState);
+    expect(nextState).toBe('Bismillah GAGAL!');
   });
 
   // Test Case 2: SET_AUTH_USER (Login)
